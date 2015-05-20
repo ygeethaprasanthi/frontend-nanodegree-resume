208b090b563de08b08ce85421df1050fefeee8fe
ygeethaprasanthi <y.geethu5@gmail.com>
2015-05-19 21:31:18 -0400

    Update resumeBuilder.js

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -2,4 +2,5 @@ var awesomethoughts = " i am geetha and i am AWESOME!";
 console.log(awesomethoughts);
 var funthoughts = awesomethoughts.replace("awesome","fun");
 console.log(funthoughts);
-var formattedname = HTMLheaderName.replace("%data%","name");
+var formattedname = HTMLheaderName.replace("%data%",name);
+var formattedrole = HTMLheaderRole.replace("%data",role);
